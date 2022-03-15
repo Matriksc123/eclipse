120b0e314910b4ed9205b6bd73866b16d2fb5e48
Matriksc123 <avismendez725@gmail.com>
2022-03-15 17:58:49 +0100

    ugkje

---
+++ HelloWorld/src/OtraCosa.java
@@ -0,0 +1,4 @@
+
+public class OtraCosa {
+public static String frase = "Hi World!";
+}
---
+++ HelloWorld/src/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OtraCosa frase = new OtraCosa();
+		System.out.println(OtraCosa.frase);
+	}
+
+}
